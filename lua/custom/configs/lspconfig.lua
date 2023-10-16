73d9accbd90ac00f39415bbfc27cb5594188d7c7
bwbonanno <bradfordbonanno@gmail.com>
2023-10-16 07:42:59 -0700

    Initial setup

---
+++ lua/custom/configs/lspconfig.lua
@@ -0,0 +1,26 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+-- if you just want default config for the servers then put them in a table
+local servers = { "html", "cssls", "tsserver", "clangd" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+local rt = require("rust-tools")
+
+rt.setup({
+  server = {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+})
+
+-- 
+-- lspconfig.pyright.setup { blabla}
